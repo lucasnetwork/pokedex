5e4f16c4fa1a1a0233b579f4bc0190ddd9c23519
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-01-24 20:50:10 -0300

    structure header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,19 @@
 import Container from "./styles";
+import { AiOutlineSearch } from "react-icons/ai";
+import logo from "../../assets/logo.png";
+import addPokemon from "../../assets/addPokemon.png";
 
-const Header = () => <Container />;
+const Header = () => (
+  <Container>
+    <img className="logo" src={logo} />
+    <div className="search_container">
+      <input placeholder="Pesquisar" />
+      <AiOutlineSearch />
+    </div>
+    <button>
+      <img className="add" src={addPokemon} />
+    </button>
+  </Container>
+);
 
 export default Header;
--- frontend/src/components/Header/styles.ts
+++ frontend/src/components/Header/styles.ts
@@ -3,6 +3,50 @@ import styled from "styled-components";
 export default styled.header`
   width: 100%;
   height: 7.5rem;
-
   background-color: ${({ theme }) => theme.background.primary};
+  display: flex;
+  padding: 0 2rem;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    object-fit: contain;
+  }
+
+  .logo {
+    width: 5.25rem;
+    margin-right: 2rem;
+  }
+
+  .add {
+    margin-left: 2rem;
+    width: 3.5rem;
+  }
+
+  button {
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+
+  .search_container {
+    display: flex;
+    flex: 1;
+    background: #fff;
+    height: 2.25rem;
+    align-items: center;
+    justify-content: center;
+    border-radius: 16px;
+    padding-right: 1rem;
+
+    input {
+      flex: 1;
+      align-self: stretch;
+      border-radius: 16px;
+      padding-left: 1rem;
+      border: none;
+      outline: none;
+    }
+  }
 `;
